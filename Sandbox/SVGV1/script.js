10df48c0a5a7f9cffe301f1d4e74d343e512e8dd
sumayyah <sumayyah16.m@gmail.com>
2014-09-16 17:03:50 -0700

    SVG framework + added Snap.svg library
    Loads SVG files; parses into variables and selectors; applies hover and
    click effects

--- Sandbox/SVGV1/script.js
+++ Sandbox/SVGV1/script.js
@@ -1,17 +1,90 @@
 $(document).ready(function(){
 
-	var offsetvar = 1; 
-
 	console.log("Ready!");
 
-	$("#content").load("timeline_svg_modified.svg", function(res){
-		if(!res){
-			console.log("Error loading svg!");
-		}
-		console.log("Loaded svg!");
-		$(this).addClass("svgLoaded");
+	/***** ANIMATE SVG *****/
 
+	var paper = Snap("#paper");
+
+	Snap.load("images/timeline_svg_modified.svg", onSVGLoaded);
+
+	var yearsGroup;
+	var entitiesGroup;
+	var pathsGroup;
+
+
+	var sixties;
+	var saul;
+
+	function onSVGLoaded(svgFile){
+
+		//Parse SVG file
 		
-	})
+		console.log("Loeaded!");
+		
+		var whole = svgFile.select("#Layer_1");
+		yearsGroup = whole.select("#Years");
+		entitiesGroup = whole.select("#Entities");
+		pathsGroup = whole.select("#Connections");
+
+		saul = entitiesGroup.select("#saul");
+		sixties = yearsGroup.select("#_x31_960s");
+
+		//Define functionalities for each section
+		// yearsGroup.hover(onhover, outhover);
+		sixties.click(yearclicktest);
+		saul.click(entityClickTest);
+
+		//Change cursor to indicate clickability
+		entitiesGroup.hover(makeClickable(entitiesGroup));
+		pathsGroup.hover(makeClickable(pathsGroup));
+
+		//Add to DOM
+		paper.append(svgFile);
+	}
+
+	var makeClickable = function(name){
+		console.log("hovered!!");
+		name.addClass("hovered");
+
+		if(name == 'pathsGroup'){ //This is not working 
+			console.log("pathsgorup!");
+			saul.attr({strokeWidth: 4});
+		}
+	}
+	var entityClickTest = function(e){
+		console.log("Clicked saul!");
+		animateHiddenDiv(e);
+	}
+
+	var yearclicktest = function(){
+		console.log("clicked!");
+		sixties.animate({transform: 's2r45,150,150'}, 1000, mina.bounce);
+	}
+	var onhover = function(){
+		console.log("Hover!");
+		yearsGroup.animate({transform: 's2r45,150,150'}, 1000, mina.bounce);
+	}
+
+	var outhover = function(){
+		console.log("Out hover!");
+		yearsGroup.animate({transform: 's1r0,150,150'}, 1000, mina.bounce);
+	}
+
+
+
+	function animateHiddenDiv(e){
+		e.preventDefault();
+		console.log("Animate hidden div");
+
+		if($('#popupDiv').is(":visible")){
+			console.log("div visible");
+			$('#popupDiv').slideUp(2000);
+		} else { 
+			console.log("div gone");
+			$('#popupDiv').slideDown(2000);
+		}
+	}
+
 
 });
